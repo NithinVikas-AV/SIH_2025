d2045e4dd9ba1d0659066acb26c16361410f0bbd
Nithin Vikas <nithinvikas8389@gmail.com>
2025-09-12 01:09:24 +0530

    fix: Just a small function addition.

--- MCP/mcp_server.py
+++ MCP/mcp_server.py
@@ -6,8 +6,6 @@ import os
 from dotenv import load_dotenv
 from mcp.server.fastmcp import FastMCP
 
-from prompt_engineering import *
-
 load_dotenv()
 MEDICAL_BOT = os.getenv("MEDICAL_BOT")
 
@@ -28,21 +26,13 @@ def stop_ollama_model(model_name):
 
 @mcp.tool(description="Provide mental health support or guidance based on user queries.")
 async def get_medical_response(query: str) -> str:
-
+    # return "Tool called successfully."
     try:
         client = ollama.AsyncClient(timeout=180)
         
-        new_query = f"""
-        Instruction before answering:
-            {medical_prompt()}
-
-        Query:
-            {query}
-        """
-
         result = await client.generate(
             model=MEDICAL_BOT,
-            prompt=new_query
+            prompt=query
         )
         stop_ollama_model(MEDICAL_BOT)
         return result.get("response", "No response from model.")
@@ -55,11 +45,13 @@ async def get_medical_response(query: str) -> str:
     Uses the anonymous id of the user to fetch his real data to send it to the counsellor in case of emergency.
 """
 
-# @mcp.tool(description="Notify counsellors if the user's mental condition is severe.")
-# def notify_counsellor():
-"""
-    Tool to notify the available counsellor for emergency situation.
-"""
+@mcp.tool(description="Notify counsellors if the user's mental condition is severe.")
+def crisis_alert():
+    """
+        Tool to notify the available counsellor for emergency situation.
+    """
+    return "Crisis Alert called."
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
